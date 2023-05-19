e3bd4fd0ce0acc75b397e10dffef5b7546e7833b
agree <ks3319t@gre.ac.uk>
2023-05-19 13:04:01 +0300

    posts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 function init(){
     const userList = document.querySelector('#user-list');
+    const postList = document.querySelector('#posts-list');
     const userDataList = document.querySelector('#user-data');
     getUsers(userList);
-    getUserData(userDataList);
+    getPosts(postList);
 }
 
 init();
@@ -13,19 +14,56 @@ async function getUsers(userList){
     const users = await fetch(`https://jsonplaceholder.typicode.com/users?_embed=posts`);
     const data = await users.json();
 
-    data.forEach(async data => {
-        const liElement = document.createElement('li');
-        const linkElement = document.createElement('a');
-        console.log(data);
-        const userID = data.id;
-        const userName = data.name;
-        const totalPosts = data.posts.length;
-        liElement.appendChild(linkElement);
-        linkElement.setAttribute('href', `user.html?id=${userID}`);
-        linkElement.textContent = `${userName} (${totalPosts})`;
-        
-        userList.append(liElement);
-    });
+    if(userList) {
+
+        data.forEach(async (data) => {
+            const liElement = document.createElement('li');
+            const linkElement = document.createElement('a');
+            const userID = data.id;
+            const userName = data.name;
+            const totalPosts = data.posts.length;
+            liElement.appendChild(linkElement);
+            linkElement.setAttribute('href', `user.html?id=${userID}`);
+            linkElement.textContent = `${userName} (${totalPosts})`;
+            
+            userList.append(liElement);
+        });
+    }
+
     
+}
+
+async function getPosts(postList) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?_expand=user`);
+    const data = await posts.json();
+
+    if(postList) {
+
+        let count = 1;
+
+        data.forEach(async (data) => {
+
+            const liElement = document.createElement('li');
+            const linkElement = document.createElement('a');
+            const authorElement = document.createElement('span');
+            const authorLink = document.createElement('a');
+ 
+            
+            authorLink.setAttribute('href', './user.html');
+            linkElement.setAttribute('href', './post.html');
+            authorElement.appendChild(authorLink);
+            authorElement.classList.add('author');
+            
+            liElement.append(linkElement, authorElement);
+           
+            const postTitle = data.title;
+            const author = data.user.name;
+            linkElement.textContent = `[${count++}] - ${postTitle}`;
+            authorLink.textContent = ` - by ${author}`;
+    
+            postList.append(liElement);
+        });
+    }
+
 }
 
